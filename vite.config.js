427feffca7fa02f5cf82ba3ed85d6f5decf8d0d9
sathieshsk398-creator <sathieshsk398@gmail.com>
2026-06-28 10:20:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: true // இது எல்லா ஹோஸ்ட்டையும் அலோவ் பண்ணிடும்!
+  }
 })
